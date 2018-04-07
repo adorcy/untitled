fe8365e8384a3ce04548eee1a9c1cb12e1fe59bf
adorcy <adorcy@unomaha.edu>
2018-04-06 22:15:21 -0500

    reverse changes

--- untitled/settings.py
+++ untitled/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
